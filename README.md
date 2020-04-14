16bd53af1f90a1f5071b3d7dc235336bccbe07f9
Nicole Gathany <nicole.gathany@gmail.com>
2020-04-14 02:36:44 -0400

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# is-happy
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,26 @@
+var isHappy = function(n) {
+  let beenHereBefore = {};
+  while (n !== 1 && !beenHereBefore[n]) {
+    beenHereBefore[n] = true;
+    n = sumSqDigits(n);
+  }
+  console.log(beenHereBefore);
+  return n === 1 ? true : false;
+};
+
+let sumSqDigits = function(number) {
+  return number
+    .toString()
+    .split("")
+    .map(a => parseInt(a, 10))
+    .reduce(function(sum, num) {
+      return sum + Math.pow(num, 2);
+    }, 0);
+};
+
+//should return true
+console.log(isHappy(19), isHappy(23), isHappy(7));
+console.log(isHappy(1));
+
+//should return false
+console.log(isHappy(2));
